a71ce1eb168ac254f9e231c885666821aaa46c38
clofolnet <cakeintheroom@gmail.com>
2023-02-26 20:28:38 +0500

    Added users tests

--- users/tests/test_api.py
+++ users/tests/test_api.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
 from users.factories import AdminUserFactory, SuperUserFactory
+from users.serializers import AdminUserSerializer
 
 
 class JwtAPITest(APITestCase):
@@ -35,6 +36,7 @@ class AdminUserAPITest(APITestCase):
     def setUp(self) -> None:
         self.super_admin = SuperUserFactory.create()
         self.admin = AdminUserFactory.stub()
+        self.created_admin = AdminUserFactory.create()
         self.payload_data = {
             'username': self.admin.username,
             'password': self.admin.password,
@@ -50,3 +52,27 @@ class AdminUserAPITest(APITestCase):
         response.force_authenticate(self.super_admin)
         response_data = response.post(url, self.payload_data)
         self.assertEqual(response_data.status_code, status.HTTP_201_CREATED)
+
+    def test_get_all_object(self):
+        url = reverse('admin-users-list')
+        response = APIClient()
+        response.force_authenticate(self.super_admin)
+        response_data = response.get(url)
+        self.assertEqual(response_data.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_data.json()), 2)
+
+    def test_retrive(self):
+        url = reverse('admin-users-detail', args={self.created_admin.id})
+        response = APIClient()
+        response.force_authenticate(self.super_admin)
+        response_data = response.get(url)
+        self.assertEqual(response_data.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data.json()['id'], self.created_admin.id)
+
+    def test_delete(self):
+        url = reverse('admin-users-detail', args={self.created_admin.id})
+        response = APIClient()
+        response.force_authenticate(self.super_admin)
+        response_data = response.delete(url)
+        self.assertEqual(response_data.status_code, status.HTTP_204_NO_CONTENT)
+
